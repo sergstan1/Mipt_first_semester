e1c5ef2cee30d51e2f8c9387f561dc75d2150df8
sergstan1 <70322617+sergstan1@users.noreply.github.com>
2020-10-05 14:25:26 +0300

    Add files via upload

---
+++ Onegin v1/Header.h
@@ -0,0 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXLEN 10000
+#define MAXLINES 5000
+#define STACKOVERWHELM -100
+#define GPO_WRONGINPUT -10
+#define MAXSIZE 100
+
+
+//--------------------------------------------------
+//! Shows how many errors were in Getpoemline
+//--------------------------------------------------
+
+int Number_of_errors = 0;
+
+
+//--------------------------------------------------
+//! If > 1, programm returns an error
+//--------------------------------------------------
+
+bool Error_warning = false;
+
+
+//--------------------------------------------------
+//! Shows how many lines were readen
+//--------------------------------------------------
+
+int Numberlines = 0;
+
+
+//--------------------------------------------------
+//! Massive, where we save pointers to lines
+//--------------------------------------------------
+
+char* Lineptr[MAXLINES];
+
+
+//--------------------------------------------------
+//! Saves line from poem
+//!
+//! @param[in] limit maximum length of line
+//! @param[in] fp pointer to file
+//! @param[out] pline pointer to Lineptr
+//!
+//! @return length of line or GPO_WRONGINPUT if input is incorrect
+//!
+//! @note if wrong input prints line where was an error
+//--------------------------------------------------
+
+int Getpoemline(char* pline, const int limit, FILE* fp);
+
+
+//--------------------------------------------------
+//! Reads lines in Lineptr[]
+//!
+//! @param[in] maxlines maximum number of lines
+//! @param[in] *fp pointer to file
+//! @param[out] Lineptr[] massive where we read
+//! 
+//! @return number of lines or STACKOVERWHELM if too many lines
+//--------------------------------------------------
+
+int Readlines(char* Lineptr[], const int maxlines, FILE* fp);
+
+
+//--------------------------------------------------
+//! prints lines from Lineptr[]
+//!
+//! @param[in] Lineptr[] what we print
+//! @param[in] Numberlines number of lines
+//!
+//! @return void
+//--------------------------------------------------
+
+void Writelines(char* Lineptr[], int Numberlines);
+
+
+
+//--------------------------------------------------
+//! strcmp from <stdlib.h>, but input is const void*, const void*
+//--------------------------------------------------
+
+int Strcmp(const void *s1, const void *s2);
+
---
+++ Onegin v1/Onegin.cpp
@@ -0,0 +1,140 @@
+#include "Header.h"
+
+int main(void)
+{
+	printf("# Sort Eugeniy Onegin\n");
+	printf("# (c) Sergey Stanko\n\n");
+
+	printf("Enter file name:\n");
+
+	FILE* fp;
+
+	char filename[MAXSIZE];
+
+	scanf("%s", filename);
+
+	fp = fopen(filename, "r");
+
+	int Numberlines = 0;
+
+	if (fp == NULL)
+	{
+		printf("Cannot read file\n");
+		return 1;
+	}
+	else if ((Numberlines = Readlines(Lineptr, MAXLINES, fp)) >= 0)
+	{
+		fclose(fp);
+
+		if (Error_warning)
+		{
+			printf("Cannot compare lines in a file\n");
+			return 1;
+		}
+
+		qsort(Lineptr, Numberlines, sizeof(*Lineptr), Strcmp);
+
+		printf("The sorted poem:\n");
+
+		Writelines(Lineptr, Numberlines);
+		return 0;
+	}
+	else
+	{
+		printf("ERROR, Too many lines\n");
+		return 1;
+	}
+}
+
+
+int Readlines(char* Lineptr[], const int maxlines, FILE* fp)
+{
+	int len = 0;
+	char line[MAXLEN];
+	char* pline;
+
+	while ((len = Getpoemline(line, MAXLEN, fp)) > 0 || len == GPO_WRONGINPUT)
+	{
+		if (len == GPO_WRONGINPUT)
+		{
+			continue;
+		}
+		if (Numberlines > maxlines || (pline = (char*)(malloc(sizeof(char) * MAXLEN))) == NULL)
+		{
+			return STACKOVERWHELM;
+		}
+		else
+		{
+			strcpy(pline, line);
+			Lineptr[Numberlines++] = pline;
+		}
+	}
+	return Numberlines;
+}
+
+
+int Getpoemline(char* pline, const int limit, FILE* fp)
+{
+
+	int c = '0';
+	int len = 0;
+
+	while ((c = getc(fp)) != EOF && len < limit && c != '\n')
+	{
+		if ((unsigned char)isalnum(c) || (unsigned char)isprint(c) || (unsigned char)isspace(c))
+		{
+			*pline = c;
+			len++;
+			pline++;
+		}
+		else
+		{
+			pline -= len;
+			Number_of_errors++;
+			Error_warning = true;
+			return GPO_WRONGINPUT;
+		}
+	}
+
+	if (c == '\n' && Number_of_errors > 0)
+	{
+		Numberlines++;
+		printf("ERROR, Wrong input in line %d\n", Numberlines);
+
+		Number_of_errors = 0;
+
+		return  GPO_WRONGINPUT;
+	}
+
+	*pline = '\0';
+
+	return len;
+}
+
+
+void Writelines(char *Lineptr[], int nlines)
+{
+	int i;
+	for (i = 0; i < nlines; i++)
+		printf("%s\n", Lineptr[i]);
+}
+
+int Strcmp(const void *s1, const void *s2)
+{
+	assert(s1 != NULL);
+	assert(s2 != NULL);
+
+	char* temp1 = *((char **)s1);
+	char* temp2 = *((char **)s2);
+
+	for (; *temp1 == *temp2; temp1++, temp2++)
+	{
+		if (*temp2 == '\0')
+			return 0;
+	}
+	return *temp1 - *temp2;
+}
+
+
+
+
