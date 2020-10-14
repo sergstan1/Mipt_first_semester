9fd42cf570bba9d76dc0453bd44b75f9377e60f5
sergstan1 <70322617+sergstan1@users.noreply.github.com>
2020-10-14 14:51:23 +0300

    Update Onegin.cpp

--- Onegin v1/Onegin.cpp
+++ Onegin v1/Onegin.cpp
@@ -2,138 +2,249 @@
 
 int main(void)
 {
+	setlocale(LC_ALL, "Rus");
+
 	printf("# Sort Eugeniy Onegin\n");
 	printf("# (c) Sergey Stanko\n\n");
 
 	printf("Enter file name:\n");
 
-	FILE* fp;
-
-	char filename[MAXSIZE];
+	char filename[MAXSIZE] = {};
 
 	scanf("%s", filename);
 
-	fp = fopen(filename, "r");
+	FILE* fp = fopen(filename, "r+b");
 
 	if (fp == NULL)
 	{
-		printf("Cannot read file\n");
+		printf("ERROR: Cannot read file\n");
 		return 1;
-	}
-	else if ((Numberlines = Readlines(Lineptr, MAXLINES, fp)) >= 0)
-	{
-		fclose(fp);
-
-		if (Error_warning)
-		{
-			printf("Cannot compare lines in a file\n");
-			return 1;
-		}
-
-		qsort(Lineptr, Numberlines, sizeof(*Lineptr), Strcmp);
-
-		printf("The sorted poem:\n");
-
-		Writelines(Lineptr, Numberlines);
-		return 0;
 	}
 	else
 	{
-		printf("ERROR, Too many lines\n");
-		return 1;
-	}
-}
+		char* text = (char*)calloc(MAXLEN, sizeof(char));
 
-
-int Readlines(char* Lineptr[], const int maxlines, FILE* fp)
-{
-	int len = 0;
-	char line[MAXLEN];
-	char* pline;
-
-	while ((len = Getpoemline(line, MAXLEN, fp)) > 0 || len == GPO_WRONGINPUT)
-	{
-		if (len == GPO_WRONGINPUT)
+		if (text == NULL)
 		{
-			continue;
+			printf("ERROR: could not allocate memory to text\n");
+
+			return 1;
 		}
-		if (Numberlines > maxlines || (pline = (char*)(malloc(sizeof(char) * MAXLEN))) == NULL)
+
+		int nsymb = Getpoem(fp, text);
+
+		if (nsymb == GP_WRONGINPUT)
 		{
-			return STACKOVERWHELM;
+			printf("ERROR: Programm couldn't read file");
+
+			return 1;
 		}
+
+		else if (nsymb == STACKOVERWHELM)
+		{
+			printf("ERROR: Too many symbols");
+
+			return 1;
+		}
+
+		else if (nsymb == ERROR_END_OF_FILE)
+		{
+			printf("ERROR: Couldn't count bytes to end of a file\n");
+
+			return -1;
+		}
+
 		else
 		{
-			strcpy(pline, line);
-			Lineptr[Numberlines++] = pline;
+			char** Lineptr_original = (char**)calloc(MAXLINES, sizeof(char*));
+			char** Lineptr_reverse = (char**)calloc(MAXLINES, sizeof(char*));
+			char** Lineptr = (char**)calloc(MAXLINES, sizeof(char*));
+
+
+			if (Lineptr_original == NULL || Lineptr_reverse == NULL || Lineptr == NULL)
+			{
+				printf("ERROR: Could not allocate memory to Lineptr\n");
+
+				return 1;
+			}
+
+			int i = 0;
+			int nlines = Getptr(Lineptr_original, text);
+
+			for (; i < nlines; i++)
+			{
+				Lineptr[i] = Lineptr_original[i];
+				Lineptr_reverse[i] = Lineptr_original[i];
+			}
+
+			qsort(Lineptr, nlines, sizeof(char*), Strcmp);
+			qsort(Lineptr_reverse, nlines, sizeof(char*), Strcmp_reverse);
+
+			fclose(fp);
+			fp = fopen(filename, "w");
+
+			fprintf(fp, "Sorted poem:\n");
+			Writelines(Lineptr, nlines, fp);
+
+			fprintf(fp, "Sorted from back poem:\n");
+			Writelines(Lineptr_reverse, nlines, fp);
+
+			fprintf(fp, "Original poem:\n");
+			Writelines(Lineptr_original, nlines, fp);
+
+			printf("\nSuccess");
+			fclose(fp);
+
+			TestComparators;
+
+			return 0;
 		}
 	}
-	return Numberlines;
 }
 
 
-int Getpoemline(char* pline, const int limit, FILE* fp)
-{
-
-	int c = '0';
-	int len = 0;
-
-	while ((c = getc(fp)) != EOF && len < limit && (unsigned char)c != '\n')
-	{
-		if ((unsigned char)isalnum(c) || (unsigned char)isspace(c))
-		{
-			*pline = c;
-			len++;
-			pline++;
-		}
-		else if ((unsigned char)isprint(c))
-		{
-			;
-		}
-		else
-		{
-			pline -= len;
-			Number_of_errors++;
-			Error_warning = true;
-			return GPO_WRONGINPUT;
-		}
-	}
-
-	if (c == '\n' && Number_of_errors > 0)
-	{
-		Numberlines++;
-		printf("ERROR, Wrong input in line %d\n", Numberlines);
-
-		Number_of_errors = 0;
-
-		return  GPO_WRONGINPUT;
-	}
-
-	*pline = '\0';
-
-	return len;
-}
-
-
-void Writelines(char *Lineptr[], int nlines)
-{
-	int i;
-	for (i = 0; i < nlines; i++)
-		printf("%s\n", Lineptr[i]);
-}
-
-
-int Strcmp(const void *s1, const void *s2)
+int Strcmp(const void* s1, const void* s2)
 {
 	assert(s1 != NULL);
 	assert(s2 != NULL);
 
-	char* temp1 = *((char **)s1);
-	char* temp2 = *((char **)s2);
+	char* temp1 = *((char**)s1);
+	char* temp2 = *((char**)s2);
 
-	for (; *temp1 == *temp2; temp1++, temp2++)
+	for (; *temp1 == *temp2 || isspace(*temp1) || isspace(*temp2); temp1++, temp2++)
 	{
-		if (*temp2 == '\0')
+		while (isspace(*temp1))
+			temp1++;
+		while (isspace(*temp2))
+			temp2++;
+		if (*temp1 == '\0' && *temp2 == '\0')
+		{
 			return 0;
+		}
 	}
-	return *temp1 - *temp2;
+	return (*temp2 - *temp1);
+}
+
+void Writelines(char* Lineptr[], const int numberliens,  FILE* fp)
+{
+	int i;
+
+	for (i = 0; i < numberliens; i++)
+	{
+		fputs(Lineptr[i], fp);
+	}
+}
+
+
+int Getpoem(FILE* fp, char* text)
+{
+	size_t nsymb = 0;
+
+	text[0] = '\n';
+	
+	fseek(fp, 0, 2);
+
+	long end_file = ftell(fp);
+
+	if (end_file == -1L)
+		return (ERROR_END_OF_FILE);
+
+	fseek(fp, 0, 0);
+
+	if ((nsymb = fread(text + 1, sizeof(char), end_file, fp)) <= end_file)
+	{
+		if (ferror(fp))
+		{
+			return GP_WRONGINPUT;
+		}
+		else
+		{
+			assert(text != NULL);
+
+			text[nsymb + 1] = '\0';
+			return nsymb + 1;
+		}
+	}
+	else
+	{
+		return STACKOVERWHELM;
+	}
+}
+
+
+int Getptr(char* Lineptr[], char* text)
+{
+	int i = 0;
+	char* pos = strtok(text, "\r");
+
+	while (pos != NULL)
+	{
+		Lineptr[i++] = pos;
+		pos = strtok(NULL, "\r");
+	}
+	return i;
+}
+
+
+int Strcmp_reverse(const void* s1, const void* s2)
+{
+	assert(s1 != NULL);
+	assert(s2 != NULL);
+
+	char* p1 = *(char**)s1;
+	char* p2 = *(char**)s2;
+
+	char* temp1 = (p1 + strlen(p1) - 1);
+	char* temp2 = (p2 + strlen(p2) - 1);
+
+	for (; *temp1 == *temp2 || isspace(*temp1) || isspace(*temp2); temp1--, temp2--)
+	{
+		while (isspace(*temp1))
+			temp1--;
+		while (isspace(*temp2))
+			temp2--;
+		if (temp1 == p1 && temp2 == p2)
+		{
+			return 0;
+		}
+	}
+	return (*temp2 - *temp1);
+}
+
+bool Test_function(const char* name, int expression)
+{
+	if (expression)
+	{
+		return true;
+	}
+	else
+	{
+		printf("test %s failed\n", name);
+		return false;
+	}
+}
+
+void TestComparators(void)
+{
+	int number_tests_passed = 0, number_tests_failed = 0;
+
+#define TEST(name, expression) ((Test_function(name, expression)) ? ++number_tests_passed : ++number_tests_failed)
+
+	TEST("Singular letters, 'a' and 'b', regular",
+		Strcmp("a", "b") > 0);
+	TEST("Singular letters, 'a' and 'b', reverse",
+		Strcmp_reverse("a", "b") > 0);
+	TEST("'abc', 'cba', regular",
+		Strcmp("abc", "cba") > 0);
+	TEST("'abc', 'cba', reverse",
+		Strcmp_reverse("abc", "cba") < 0);
+	TEST("Equal lines, 'abcd', 'abcd', regular",
+		Strcmp("abcd", "abcd") == 0);
+	TEST("Equal lines, 'abcd', 'abcd', reverse",
+		Strcmp_reverse("abcd", "abcd") == 0);
+
+#undef TEST
+
+	if (number_tests_failed != 0)
+		printf("Some units tests have failed! Number of failed tests is %d\n", number_tests_failed);
 }
